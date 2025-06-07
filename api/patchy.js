0ea6ed170813d6c70ba0807e0dd92d6a2f9c9026
GMEstonk <112446331+GMEstonk@users.noreply.github.com>
2025-06-07 11:47:47 -0500

    Update patchy.js

--- api/patchy.js
+++ api/patchy.js
@@ -19,5 +19,5 @@ function setBackgroundInterval(fn, time) {
 }
 
 
-  setBackgroundInterval(()=>[...document.querySelectorAll(`a[href="${location.origin}"]`)??[]]find?.(x=>`${x?.innerText}`.toLowerCase().includes('pokéheroes wiki'))?.setAttribute?.('href',`${location.origin}/wiki/Main_Page`),100);
+  setBackgroundInterval(()=>[...document.querySelectorAll(`a[href^="${location.origin}"]`)??[]]find?.(x=>`${x?.innerText}`.toLowerCase().includes('wiki'))?.setAttribute?.('href',`${location.origin}/wiki/Main_Page`),100);
 })();
