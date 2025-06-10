d040b0fe12280c9af84c49b88b0b9693cbe5c5bf
GMEstonk <112446331+GMEstonk@users.noreply.github.com>
2025-06-10 09:06:14 -0500

    Update patchy.js

--- api/patchy.js
+++ api/patchy.js
@@ -1,91 +1,115 @@
-(()=>{
-  const _eval = eval;
-  globalThis.eval = function eval(){
-    try{
-      return _eval(...arguments);
-    }catch(e){
-      console.warn(e,...arguments);
-    }
-  };
+(() => {
+    const setBackgroundInterval = function setBackgroundInterval(fn, time) {
+        const requestIdleCallback =
+            globalThis.requestIdleCallback ?? globalThis.requestAnimationFrame;
+        let running = false;
+        return setInterval(() => {
+            if (running) return;
+            running = true;
+            requestIdleCallback(async () => {
+                try {
+                    await fn();
+                } catch (e) {
+                    console.warn(e);
+                } finally {
+                    running = false;
+                }
+            });
+        }, time);
+    };
+
+  const updateAttribute = (element,key,value) => (element?.getAttribute?.(key) != value) && element?.setAttribute?.(key,value);
+
+    (() => {
+        const _eval = eval;
+        globalThis.eval = function eval() {
+            try {
+                return _eval(...arguments);
+            } catch (e) {
+                console.warn(e, ...arguments);
+            }
+        };
+    })();
+
+    (() => {
+        const xhr = XMLHttpRequest.prototype;
+        const _open = xhr.open;
+        xhr.open = function open() {
+            try {
+                arguments[1] &&= String(arguments[1]).replace(RegExp(atob('cG9rZWhlcm9lcy5jb20='), 'gi'), location.host);
+            } catch (e) {
+                console.warn(this, e, ...arguments);
+            }
+            return _open.apply(this, arguments);
+        };
+    })();
+
+    (() => {
+        const dateParse = Date.parse;
+        Date.parse = function parse() {
+            try {
+                return dateParse(...arguments) || new Date();
+            } catch {
+                return new Date();
+            }
+        };
+    })();
+
+    (() => {
+        const jsonParse = JSON.parse;
+        JSON.parse = function parse() {
+            try {
+                return jsonParse(...arguments);
+            } catch (e) {
+                return e;
+            }
+        };
+    })();
+
+    (() => {
+        setBackgroundInterval(() => [...document.querySelectorAll(`a[href="//${location.host}"]`) ?? []]?.find?.(x => `${x?.innerText}`.toLowerCase().includes('wiki'))?.setAttribute?.('href', `${location.origin}/wiki/Main_Page`), 100);
+    })();
+
+    (() => {
+        function cssHelpers() {
+            const html = document.firstElementChild;
+
+            const toKebabCase = x =>
+                String(x).replace(/[A-Z]+/g, y => `-${y.toLowerCase()}`).replace(/[^a-z0-9-]/g, '').replace(/[-]+/g, '-').replace(/^-/, '');
+
+            const isString = str => str instanceof String || [typeof str, str?.constructor?.name].some(s => /^string$/i.test(s));
+
+            for (const prop in html) {
+                if (html[prop] && isString(html[prop]) && !/\n/.test(html[prop])) {
+                    updateAttribute(html,`html-${toKebabCase(prop)}`.replace(/[-]+/g, '-'), html[prop]);
+                }
+            }
+
+            for (const obj of [document, window, location, navigator, clientInformation.userAgentData]) {
+                const prefix = `${obj?.constructor?.name}`.replace(/^html/i, '').toLowerCase();
+                for (const prop in obj) {
+                    if (obj[prop] && isString(obj[prop])) {
+                        updateAttribute(html,`${toKebabCase(prefix)}-${toKebabCase(prop)}`.replace(/[-]+/g, '-'), obj[prop]);
+                    }
+                }
+            }
+
+            const loc = new URL(location.href);
+            for (const [k, v] of loc.searchParams) {
+                if (k && v) {
+                    updateAttribute(html,`location-search-params-${toKebabCase(k)}`.replace(/[-]+/g, '-'), v);
+                }
+            }
+
+            const cookies = new URLSearchParams(`?${`${document?.cookie}`.split('; ').join('&')}`);
+            for (const [k, v] of cookies) {
+                if (k && v) {
+                    updateAttribute(html,`cookie-${toKebabCase(k)}`.replace(/[-]+/g, '-'), v);
+                }
+            }
+        }
+        cssHelpers();
+      setBackgroundInterval(cssHelpers,100);
+    })();
+
 })();
-
-(()=>{
-  const xhr = XMLHttpRequest.prototype;
-  const _open = xhr.open;
-  xhr.open = function open(){
-    try{
-      arguments[1] &&= String(arguments[1]).replace(RegExp(atob('cG9rZWhlcm9lcy5jb20='),'gi'),location.host);
-    }catch(e){
-      console.warn(this,e,...arguments);
-    }
-    return _open.apply(this,arguments);
-  };
-})();
-
-(()=>{
-  const dateParse = Date.parse;
-  Date.parse = function parse(){
-    try{
-      return dateParse(...arguments) || new Date();
-    }catch{
-      return new Date();
-    }
-  };
-})();
-
-(()=>{
-  const jsonParse = JSON.parse;
-  JSON.parse = function parse(){
-    try{
-      return jsonParse(...arguments);
-    }catch(e){
-      return e;
-    }
-  };
-})();
-
-(()=>{
-function setBackgroundInterval(fn, time) {
-  const requestIdleCallback =
-    globalThis.requestIdleCallback ?? globalThis.requestAnimationFrame;
-  let running = false;
-  return setInterval(() => {
-    if (running) return;
-    running = true;
-    requestIdleCallback(async () => {
-      try {
-        await fn();
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        running = false;
-      }
-    });
-  }, time);
-}
-  setBackgroundInterval(()=>[...document.querySelectorAll(`a[href="//${location.host}"]`)??[]]?.find?.(x=>`${x?.innerText}`.toLowerCase().includes('wiki'))?.setAttribute?.('href',`${location.origin}/wiki/Main_Page`),100);
-})();
-
-(()=>{
-  const html = document.firstElementChild;
-  const toKebabCase = x =>
-    String(x).replace(/[A-Z]+/g, y => `-${y.toLowerCase()}`).replace(/[^a-z0-9-]/g,'').replace(/[-]+/g,'-');
-  const isString = str => str instanceof String || [typeof str,str?.constructor?.name].some(s=>/^string$/i.test(s));
-  for(const obj of [document,window,location,navigator]){
-    const prefix = `${obj?.constructor?.name}`.replace(/^html/i,'').toLowerCase();
-    for(const prop in obj){
-      if(obj[prop] && isString(obj[prop])){
-        html.setAttribute(`${prefix}-${toKebabCase(prop)}`,obj[prop]);
-      }
-    }
-  }
-  const loc = new URL(location.href);
-  for(const [k,v] of loc.searchParams){
-    if(k && v){
-      html.setAttribute(`${location-search-params}-${toKebabCase(k)}`,v);
-    }
-  }
-})();
-
-
-
